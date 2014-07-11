faf98e09643e677001165ccea29a447b0005138b
sharathts <ts.sharath18@gmail.com>
2014-07-12 01:25:01 +0530

    trying to train

--- blacken_palm.m
+++ blacken_palm.m
@@ -11,10 +11,10 @@ img = ind2gray (x, map);
 img = img / 255.0;
 
 #Pass the image through a low pass filter, this is done to make the size reduction more smooth, thus retaining a lot of info
-low_pass = zeros(3, 3) .+ (1 / 9);
-im1 = img;
-im1 = imfilter(im1, low_pass, "replicate");
-img = im1;
+#low_pass = zeros(3, 3) .+ (1 / 9);
+#im1 = img;
+#im1 = imfilter(im1, low_pass, "replicate");
+#img = im1;
 
 #Reduce the size of the image
 img = imresize(img, [frame_size frame_size]);
@@ -22,8 +22,8 @@ img = imresize(img, [frame_size frame_size]);
 #Threshold the image, such that the hand is black and the background is white
 #white is 1.0 and black is 0.0
 
-img(img < 0.4 | img >= 0.75) = 1.0;
-img(img < 0.75 & img >= 0.4) = 0.0;
+img(img < 0.4 | img >= 0.9) = 1.0;
+img(img < 0.9 & img >= 0.4) = 0.0;
 
 #What you are doing now is, you took the colour of blue, then suppressed blue to white and remaining to black right? Hence its limited to blue background.
 #What if we take the gray scale value for skin colour and then make all the values in the range of skin colour to be black, remaining as white. Times when this would fail is if he is wearing gloves or if the background is very much like skin colour, obviously the chances of this happening is lower than previous.
--- main.m
+++ main.m
@@ -2,22 +2,42 @@
 max_row = 1000;
 max_col = 1000;
 frame_size = 100;
+flag = 0;
+base_a = "A";
+base_b = "B";
+K = 2;
+training_size = 8;
+test_size = 6;
 
+total = frame_size * K;
+X = zeros(training_size, total);
+%y = [2;2;2;2;2;2;2;2];
+
+#Training
+for iter = 1:training_size,
 #Reads the Image
-img = imread ("hand.jpg");
-#img = imread ("blue.jpg");
-#img = imread ("palm_white.jpg");
-#size(img)
+if flag == 0
+    key = (iter + 1) / 2;
+    key = num2str(key);
+    file = strcat(base_a, key, ".jpeg");
+    flag = 1;
+else
+    key = iter / 2;
+    key = num2str(key);
+    file = strcat(base_b, key, ".jpeg");
+    flag = 0;
+end;
+
+img = imread (file);
 
 #Blackens the palm
 [img] = blacken_palm(img, frame_size);
 #imshow(img); #undocument to see blackened hand
-##pause;
+#pause;
 
 ##Filters the image
-
 #frame_size = 80;
-img = crop_hand(img, frame_size);
+#img = crop_hand(img, frame_size);
 #imshow(img);
 
 #Applying a high pass filter, since it is used for edge detection, seems to do a better job
@@ -27,34 +47,81 @@ img = crop_hand(img, frame_size);
 
 #Does featureNormalize
 [img_norm, mu, sigma] = featureNormalize(img);
-imshow(img_norm); #Undocument to see the normalised hand
+#imshow(img_norm); #Undocument to see the normalised hand
 #pause;
 
-disp("Now starting pca");
+#disp("Now starting pca");
 [U, S] = pca(img_norm, frame_size, frame_size);
-disp("Done with pca");
-K = 30;
+#disp("Done with pca");
 Z = projectData(img_norm, U, K);
 #imshow(Z); #Undocument to see pca
 
-total = frame_size * K;
-
-X = zeros(2, total);
-
 #Have to decide on C and sigma values
-C = 1;
+#C = 1;
 %sigma = 0.3;
-sigma = 0.1;
+#sigma = 0.1;
 
 #putting all features into a row vector
 #but does this make sense? After applying PCA in the way we did apply
 for index = 1:total
-    X(1,index) = Z(index);
-    X(2,index) = Z(index) + 0.5;
+    X(iter,index) = Z(index);
     end;
-y = [1;0]; 
-#X = [1,2;3,4];   
-model = svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2,sigma));
-predictions = svmPredict(model, X);
-disp(predictions);
+end;
+C = 0.1;
+model = svmTrain(X, y, C, @linearKernel);
+
+#Testing
+flag = 0;
+X_test = zeros(test_size, total);
+for iter = (training_size+1):(test_size+training_size),
+#Reads the Image
+if flag == 0
+    key = (iter + 1) / 2;
+    key = num2str(key);
+    file = strcat(base_a, key, ".jpeg");
+    flag = 1;
+else
+    key = iter / 2;
+    key = num2str(key);
+    file = strcat(base_b, key, ".jpeg");
+    flag = 0;
+end;
+
+img = imread (file);
+
+#Blackens the palm
+[img] = blacken_palm(img, frame_size);
+#imshow(img); #undocument to see blackened hand
+#pause;
+
+##Filters the image
+#frame_size = 80;
+#img = crop_hand(img, frame_size);
+#imshow(img);
+
+#Applying a high pass filter, since it is used for edge detection, seems to do a better job
+#high_pass = [0.25,0.5,0.25;0.5,9,0.5;0.25,0.5,0.25];
+#img = imfilter(img, high_pass, "replicate");
+#imshow(img); #Undocument to see filtered hand
+
+#Does featureNormalize
+[img_norm, mu, sigma] = featureNormalize(img);
+#imshow(img_norm); #Undocument to see the normalised hand
+#pause;
+
+#disp("Now starting pca");
+[U, S] = pca(img_norm, frame_size, frame_size);
+#disp("Done with pca");
+Z = projectData(img_norm, U, K);
+#imshow(Z); #Undocument to see pca
+
+#putting all features into a row vector
+#but does this make sense? After applying PCA in the way we did apply
+for index = 1:total
+    X_test((iter-8),index) = Z(index);
+    end;
+end;
+predictions = svmPredict(model, X_test);
+model
+#disp(predictions);
 ##===========================================================================================================
