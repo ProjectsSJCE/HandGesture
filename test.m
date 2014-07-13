8a1d9aa48f1aa6c597ddc8070a0dcafa621319b8
sharathts <ts.sharath18@gmail.com>
2014-07-13 14:19:44 +0530

    indented neat code

--- test.m
+++ test.m
@@ -6,91 +6,107 @@ flag = 0;
 base_a = "A";
 base_b = "B";
 K = 100;
+
+C = 0.05;# The value with least validation error
+#sigma = 
+
 training_size = 8;
 test_size = 6;
 total = frame_size * K;
-X = zeros(training_size*20, total);
-y = zeros(160,1);
-for i = 1:2:160,
+#repeat = 1;
+X = zeros(training_size, total);
+y = zeros(training_size,1);
+for i = 1:2:(training_size),
     y(i) = 1;
-    end;
-for count = 1:20,
+    endfor;
+flag = 0;
+
 #Training
 for iter = 1:training_size,
 #Reads the Image
-if flag == 0
-    key = (iter + 1) / 2;
-    key = num2str(key);
-    file = strcat(base_a, key, ".jpeg");
-    flag = 1;
-else
-    key = iter / 2;
-    key = num2str(key);
-    file = strcat(base_b, key, ".jpeg");
-    flag = 0;
-end;
+    if flag == 0
+        key = (iter + 1) / 2;
+        key = num2str(key);
+        file = strcat(base_a, key, ".jpeg");
+        flag = 1;
+    else
+        key = iter / 2;
+        key = num2str(key);
+        file = strcat(base_b, key, ".jpeg");
+        flag = 0;
+    endif;
 
-img = imread (file);
-#rgb = img;
-#[x, map] = rgb2ind(img);
-#img = ind2gray (x, map);
-#[row,col] = size(img);
-#img = img / 255.0;
-[img] = blacken_palm(img, frame_size);
-#imshow(img);
-#pause;
-#Have to decide on C and sigma values
-C = 0.05;
-#sigma = 0.3;
+    img = imread (file);
+    [img] = blacken_palm(img, frame_size);
+#    imshow(img);
+#    pause;
+
 #putting all features into a row vector
-#but does this make sense? After applying PCA in the way we did apply
-#disp((training_size*(count-1)) + iter);
-for index = 1:total
-    X((training_size*(count-1)) + iter,index) = img(index);
-    end;
-#disp("saar");
-end;
-end;
-#y = [1;0]; 
-#X = [1,2;3,4]; 
-#C = 0.03;
+    for index = 1:total
+#       X((training_size*(count-1)) + iter,index) = img(index);
+        X(iter,index) = img(index);
+    endfor;
+endfor;
+
+#Does featureNormalize
+#[img_norm, mu, sigma] = featureNormalize(X);
+#imshow(img_norm); #Undocument to see the normalised hand
+#pause;
+
+#disp("Now starting pca");
+#[U, S] = pca(img_norm, frame_size, frame_size);
+#disp("Done with pca");
+#Z = projectData(img_norm, U, K);
+#X = Z;
+#imshow(Z); #Undocument to see pca
+#pause;
+
 disp("training");
-#size(X)
 #model = svmTrain(X, y, C, @gaussianKernel);
 #model = svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma));
 model = svmTrain(X, y, C, @linearKernel);
 #model = svmTrain(X, y, C, @linearKernel, 0.1, 0.1);
 #Testing
+
 flag = 0;
 X_test = zeros(test_size, total);
-for iter = (training_size+1):(test_size+training_size),
-#Reads the Image
-if flag == 0
-    key = (iter + 1) / 2;
-    key = num2str(key);
-    file = strcat(base_a, key, ".jpeg");
-    flag = 1;
-else
-    key = iter / 2;
-    key = num2str(key);
-    file = strcat(base_b, key, ".jpeg");
-    flag = 0;
-end;
 
-img = imread (file);
-#rgb = img;
-#[x, map] = rgb2ind(img);
-#img = ind2gray (x, map);
-#[row,col] = size(img);
-#img = img / 255.0;
-[img] = blacken_palm(img, frame_size);
-#imshow(img);
+for iter = 1:test_size,
+#Reads the Image
+    if flag == 0
+        key = (iter + 1) / 2;
+        key = num2str(key);
+        file = strcat(base_a, key, ".jpeg");
+        flag = 1;
+    else
+        key = iter / 2;
+        key = num2str(key);
+        file = strcat(base_b, key, ".jpeg");
+        flag = 0;
+    endif;
+    img = imread (file);
+    [img] = blacken_palm(img, frame_size);
+    #imshow(img);
+    #pause;
+    for index = 1:total
+        X_test(iter,index) = img(index);
+    endfor;
+endfor;
+
+#Does featureNormalize
+#[img_norm, mu, sigma] = featureNormalize(X_test);
+#imshow(img_norm); #Undocument to see the normalised hand
 #pause;
-for index = 1:total
-    X_test((iter-8),index) = img(index);
-    end;
-#disp("saar2");
-end;
+
+#disp("Now starting pca");
+#PCA
+#[U, S] = pca(img_norm, frame_size, frame_size);
+#disp("Done with pca");
+#Z = projectData(img_norm, U, K);
+#imshow(Z); #Undocument to see pca
+#pause;
+#X_test = Z;
+
 disp("predicting");
 predictions = svmPredict(model, X_test);
 disp(predictions);
